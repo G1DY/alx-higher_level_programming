f7630c0dc17e034abb0127a38abcad3741ba330c
gideon <kipkerring.gedion@gmail.com>
2023-04-04 00:58:45 -0700

    calculates the Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -46,9 +46,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """returns the area of the rectangle"""
+        """evaluates the area of the rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """returns the perimeter of the rectangle"""
+        """evaluates the perimeter of the rectangle"""
         return ((self.__width * 2) + (self.__height * 2))
