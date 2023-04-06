5b4800c4a3579d598b6720c0f3429b5c8087c7f9
gideon <kipkerring.gedion@gmail.com>
2023-04-06 03:28:10 -0700

    a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines a matrix division module"""
+
+
+def matrix_divided(matrix, div):
+    """divides integer/float of a matrix
+
+    Args:
+        matrix: list of ints and floats
+        div: the divisor (int/float)
+
+    Raises:
+        TypeError: if the matrix contains non-numbers
+        TypeError: if the matrix contains rows of different sizes
+        TypeError: if divisor is not an int or float
+        ZeroDivisorError: if the denominator in the division is 0
+    Returns:
+        A new matrix with the result of division
+    """
+    if not all(isinstance(row, list) and all(isinstance(x, (int, float)) for x in row) for 
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines a function print names"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints names
+    
+    Args:
+        first_name: string of first name
+        last_name: string of second name
+
+    Raises:
+        TypeError: if first name or last name is not a string
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(second_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, second_name))
