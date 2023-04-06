ecd267a29dabf5bc60a13903b9475b40c53265a3
gideon <kipkerring.gedion@gmail.com>
2023-04-06 01:43:58 -0700

    a function magic_string() that returns a string “BestSchool” n times

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function magic_string() 
+   that returns a string “BestSchool” n times
+"""
+
+
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool") 
