11a877be544a34c93254b7ded1416dfc47e9de67
gideon <kipkerring.gedion@gmail.com>
2023-04-11 16:18:10 -0700

    a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class MyInt that inherits from int"""
+
+
+class MyInt:
+    """inverts int operators == and !="""
+
+    def __eq__(self, value):
+        """replace == with !="""
+        return self.real != value
+
+    def __ne__(self, value):
+        """replace != with =="""
+        return self.real == value
