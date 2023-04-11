5a620195701707e1e070c030f282bfd944ff7dea
gideon <kipkerring.gedion@gmail.com>
2023-04-11 15:34:08 -0700

    does area print() should print, and str() should return, the following rectangle description:

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """represents Rectangle using basegeometry"""
+    def __init__(self, width, height):
+        """initializes a rectangle
+        Args:
+            width: width of the rectangle
+            height: length of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns area of the rectangle"""
+        return self.__width * self__height
+
+    def __str__(self):
+        """returns the print and str representation of the rectangle"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
