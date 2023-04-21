514fd5c35278e0d740cdf4f4a0fff45fe7da8a6b
gideon <kipkerring.gedion@gmail.com>
2023-04-21 04:54:40 -0700

    Write the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a the first class Base"""
+
+
+class Base:
+    """Represents “base” of all other classes in this project
+       The goal of it is to manage id attribute in all the  future
+       classes and to avoid duplicating the same code 
+
+    Attribute:
+        __nb_objects = 0:number of base objects
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes the first base
+
+        Args:
+            id: an int representing id of new base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
