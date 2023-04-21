ad1da04e9fb4cd97a5cd94e65aa71d426170b680
gideon <kipkerring.gedion@gmail.com>
2023-04-21 10:05:22 -0700

    class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,3 +116,7 @@ class Rectangle(Base):
             if y < 0:
                 raise ValueError("y must be >= 0")
             self.__y = value
+
+        def area(self):
+            """returns the area of the rectangle"""
+            return (self.__width * self.__height)
