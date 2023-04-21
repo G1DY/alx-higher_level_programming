b4b89534ac76e041a2deac5700bf34cfc6fbd381
gideon <kipkerring.gedion@gmail.com>
2023-04-21 05:01:19 -0700

    Write the first class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@
 class Base:
     """Represents “base” of all other classes in this project
        The goal of it is to manage id attribute in all the  future
-       classes and to avoid duplicating the same code 
+       classes and to avoid duplicating the same code
 
     Attribute:
         __nb_objects = 0:number of base objects
