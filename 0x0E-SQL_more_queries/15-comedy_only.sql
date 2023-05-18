9c78d573f0e172e94f193e5e2aec368882a10695
Gideon <kipkerring.gedion@gmail.com>
2023-05-18 07:09:31 -0700

    a script that lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,7 +2,7 @@
 SELECT t.`title`
 FROM `tv_shows` AS s
 INNER JOIN `tv_show_genres` AS t
-ON s.`id` = t.`genre_id`
+ON s.`id` = t.`show_id`
 INNER JOIN `tv_genres` AS g
 ON g.`id` = t.`show_id`
 WHERE g.`name` = "Comedy"
