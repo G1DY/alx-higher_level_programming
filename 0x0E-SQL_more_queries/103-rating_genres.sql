8460cc5cff19c714b3af2d64b74cf06c9aa4d04b
Gideon <kipkerring.gedion@gmail.com>
2023-05-18 11:11:15 -0700

    a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,7 +4,7 @@ FROM `tv_genres` AS g
 INNER JOIN `tv_show_genres` AS s
 ON s.`genre_id` = g.`id`
 INNER JOIN `tv_show_ratings` AS r
-ON r`.show_id` = s.`show_id`
+ON r.`show_id` = s.`show_id`
 GROUP BY `name`
 ORDER BY `rating` DESC;
 
