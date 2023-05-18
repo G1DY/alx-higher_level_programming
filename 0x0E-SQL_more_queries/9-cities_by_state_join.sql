9c97f86b6939a0c5b634ede8a919f86698203abb
Gideon <kipkerring.gedion@gmail.com>
2023-05-18 05:12:22 -0700

    a script that lists all cities contained in the database hbtn_0d_usa

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name and states.name
 FROM hbtn_0d_usa.cities and hbtn_0d_usa.states
-ORDER BY ASC;
+ORDER BY cities.name ASC;
 
