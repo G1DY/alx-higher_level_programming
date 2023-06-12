379d9bb5ae128b945f33c6707204287693aeffeb
Gideon <kipkerring.gedion@gmail.com>
2023-06-12 11:29:22 -0700

    a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints My number: <first argument converted in integer>
+
+if (isNaN(process.argv[2]) || process.argv[2] === 'undefined') {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
+}
