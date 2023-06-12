b81ed4fcb30ea2d92fd9edc478549298e0294885
Gideon <kipkerring.gedion@gmail.com>
2023-06-12 11:35:34 -0700

    a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// a script that prints 3 lines: uses array string and loop
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (i = 0; i < 3; i++)
+  console.log(myString[i]);
