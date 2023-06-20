0cf88d4eeb8a8562b4727ed8dabd59ff105a4d39
Gideon <kipkerring.gedion@gmail.com>
2023-06-20 04:31:09 -0700

    a script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(db_uri)
-
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
     for instance in session.query(State).order_by(State.id):
-        print("{0} : {1}".format(instance.id, instance.name))
+        print("{0}: {1}".format(instance.id, instance.name))
