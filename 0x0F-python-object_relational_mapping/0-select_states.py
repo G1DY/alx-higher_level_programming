4acee1be1480646590f2ff478bd5cca0164110a7
Gideon <kipkerring.gedion@gmail.com>
2023-06-19 23:35:23 -0700

    a script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,17 @@ import sys
 
 
 if __name__ == "__main__":
-    db = _mysql.connect(host="localhost", user="sys.argv[1]",
-                        passwd="sys.argv[2]", db="sys.argv[3]", port=3306)
+    if len(sys.argv) != 4:
+        print("Access: python3 0-select_states.py username \
+              passwd database")
+        sys.exit(1)
+
+    password = sys.argv[1]
+    username = sys.argv[2]
+    database = sys.argv[3]
+
+    db = _mysql.connect(host="localhost", passwd=password,
+                        user=username, db=database, port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
