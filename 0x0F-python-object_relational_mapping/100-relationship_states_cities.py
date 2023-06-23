d1bae9e2fcdceb6f690e75c90147cb2f17caa8a2
Gideon <kipkerring.gedion@gmail.com>
2023-06-22 23:25:05 -0700

    a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,13 +13,14 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
 
+    Session = sessionmaker(bind=engine)
     session = Session()
+
     new_state = State(name="California")
     new_city = City(name="San Francisco")
-
     new_state.cities.append(new_city)
+
     session.add(new_state)
     session.add(new_city)
     session.commit()
