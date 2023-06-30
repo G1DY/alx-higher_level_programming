9250b20abf76c2bf5cdf727641aba22d23a744c1
Gideon <kipkerring.gedion@gmail.com>
2023-06-30 07:09:59 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
+# send a request and gets bytes size of a response header
 curl -s "$1" | wc -c
